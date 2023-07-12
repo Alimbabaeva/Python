9bd02f53785b4ba9fd665d8b8c2c3df411c8de91
Nurzat <alimbabaevanurzat@gmail.com>
2023-07-13 00:44:08 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# x=[1,2,3,4,5]
+# x.pop(2)
+# x.append("hello")
+# print (x)
+x=-1
+y=0
+z=2
+
+# X
+if x>0 and x!=0  :
+   print("Plus number")
+elif x==0:
+   print("Woow its 0")
+elif x<0 :
+  print("not Plus number")
+  
+# Y
+if y>0 and y!=0  :
+   print("Plus number")
+elif y==0:
+   print("Woow its 0")
+elif y<0 :
+  print("not Plus number")
+  
+ # Z
+if z>0 and z!=0  :
+   print("Plus number")
+elif z==0:
+   print("Woow its 0")
+elif z<0 :
+  print("not Plus number")
+
